90b2eae9d35a25b139f18fbf3cba14eaf8d3fd72
김다온 <128460925+kdaon07@users.noreply.github.com>
2024-04-17 19:12:03 +0900

    commit

--- ALOG/src/App.jsx
+++ ALOG/src/App.jsx
@@ -1,8 +1,10 @@
 import react from 'react'
 import Slide from './Slide';
+import UseAdd from './UseAdd';
 
 export default function App() {
   return (
-    <Slide/>
+    // <Slide/>
+    <UseAdd/>
   );
 }
